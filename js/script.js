dba06922413360ccf11ec43904f6c4a223d18bfa
Yoyi <johanadavilab@gmail.com>
2025-10-04 19:22:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper(".bioSwiper, .discoSwiper, .prodSwiper", {
+  rewind: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
